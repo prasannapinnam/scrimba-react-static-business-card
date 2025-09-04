2fe9a236117b4b61721525ae0a95dff7242bb0a4
prasannapinnam <prasannapinnam999@gmail.com>
2025-09-04 16:53:45 +0530

    generated dist folder and pushing it for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/prasannapinnam/scrimba-react-static-business-card',   // <-- replace with your repo name
 })
+
+
